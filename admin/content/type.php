c7115df92bf0bf3d3ef491ea30c3c8726e78be9c
EmelineDW <emeline96@hotmail.com>
2026-03-04 15:25:31 +0100

    type.php et type classe + backup

--- admin/content/type.php
+++ admin/content/type.php
@@ -1,1 +1,11 @@
 <h2>Gestion des catégories de produits</h2>
+<?php
+
+$types = new TypeDAO($cnx);
+$liste = $types->getAllTypes();
+?>
+<pre>
+    <?php
+    var_dump($liste); //var_dump affiche le contenu d'un objet
+    ?>
+</pre>
--- admin/src/php/classes/Type.class.php
+++ admin/src/php/classes/Type.class.php
@@ -1,1 +1,15 @@
 <?php
+
+declare (strict_types=1);
+
+class Type
+{
+    public function __construct(
+        public readonly int    $id_type,
+        public readonly string $nom_type
+    )
+    {
+        //gestion des exceptions
+    }
+
+}
