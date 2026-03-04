fab47f73af3101d877f968257c292f9cdab6d51d
EmelineDW <emeline96@hotmail.com>
2026-03-04 14:07:15 +0100

    essai

--- admin/src/php/classes/Connexion.class.php
+++ admin/src/php/classes/Connexion.class.php
@@ -1,1 +1,16 @@
 <?php
+
+class Connexion
+{
+    // méthode statique dont la classe ne nécesite pas d'instanciation
+    public static function getInstance($dsn, $user, $pass)
+    {
+        try {
+            $pdo = new PDO($dsn, $user, $pass);
+            var_dump($pdo); // affiche le contenu d'un objet
+            return $pdo;
+        } catch (PDOException $e) {
+            print "Erreur : " . $e->getMessage() . "<br/>";
+        }
+    }
+}
